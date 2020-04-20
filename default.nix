6918198b72075b53090ffd949d5a6fae44b0d88b
Ruud van Asseldonk <dev@veniogames.com>
2020-04-20 21:52:32 +0200

    Add Nix profile to manage development environment

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import (import ./nixpkgs-pinned.nix) {};
+in
+  pkgs.buildEnv {
+    name = "sempervivum-devenv";
+    paths = [
+      pkgs.glibcLocales # Needed for the locale-archive, to avoid LC_* errors.
+      pkgs.gmp          # Required by the Haskell runtime.
+      pkgs.stack        # Provides the Haskell toolchain.
+    ];
+  }
---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/b3c3a0bd1835a3907bf23f5f7bd7c9d18ef163ec.tar.gz";
+  sha256 = "0b9n5arqr3pzyg1ws0wf84zlczgllxnhjm1mzddd1qk5w5nw7fcy";
+}
