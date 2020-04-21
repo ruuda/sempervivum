b797599981118b8c9341de4d8081885d9210568f
Ruud van Asseldonk <dev@veniogames.com>
2020-04-21 23:17:21 +0200

    Fix bug in last watered query
    Because "id" occurs in both tables, we have to prefix it.

--- src/Database.hs
+++ src/Database.hs
@@ -75,21 +75,21 @@ listPlants conn =
       Plant (PlantId pid) (Species species) lastWatered lastFertilized
   in
     fmap decode <$> Sqlite.query conn
-      " select                                                                   \
-      \   id,                                                                    \
-      \   species,                                                               \
-      \   (                                                                      \
-      \     select max(time)                                                     \
-      \     from events                                                          \
-      \     where plant_id = id and type = 'watered'                             \
-      \   )                                                                      \
-      \   as last_watered,                                                       \
-      \   (                                                                      \
-      \     select max(time)                                                     \
-      \     from events                                                          \
-      \     where plant_id = id and type = 'fertilized'                          \
-      \   )                                                                      \
-      \   as last_fertilized                                                     \
-      \ from                                                                     \
-      \   plants;                                                                "
+      " select                                                                 \
+      \   id,                                                                  \
+      \   species,                                                             \
+      \   (                                                                    \
+      \     select max(time)                                                   \
+      \     from events                                                        \
+      \     where plant_id = plants.id and type = 'watered'                    \
+      \   )                                                                    \
+      \   as last_watered,                                                     \
+      \   (                                                                    \
+      \     select max(time)                                                   \
+      \     from events                                                        \
+      \     where plant_id = plants.id and type = 'fertilized'                 \
+      \   )                                                                    \
+      \   as last_fertilized                                                   \
+      \ from                                                                   \
+      \   plants;                                                              "
       ()
