6664a060feb89a480240596c2e39d4e3c7dc89b9
Ruud van Asseldonk <dev@veniogames.com>
2020-11-08 18:06:49 +0100

    Change adaptive interval half-life from 30d to 15d
    I noticed that with the current version, it does take a long time for
    the adaptive interval to catch up with a change in how I water, so let's
    reduce it a bit, and see how that goes.

--- app/src/Plant.purs
+++ app/src/Plant.purs
@@ -113,12 +113,12 @@ adaptiveWateringInterval (Plant p) baseInterval =
     Just watered ->
       let
         t = NonEmpty.last watered
-        -- Weigh watering events with exponential decay with a half-life of 30
-        -- days. So the last interval gets weight 1, an interval that ended 15
-        -- days before the last one gets weight 0.707, an interval that ended 30
-        -- days ago gets weight 0.5. An interval that ended 180 days ago gets
-        -- weight 0.015.
-        lambda = (Math.log 0.5) / (30.0 * 24.0 * 3600.0)
+        -- Weigh watering events with exponential decay with a half-life of 15
+        -- days. So the last interval gets weight 1, an interval that ended 7
+        -- days before the last one gets weight 0.72, an interval that ended 30
+        -- days ago gets weight 0.25. An interval that ended 100 days ago gets
+        -- weight 0.01.
+        lambda = (Math.log 0.5) / (15.0 * 24.0 * 3600.0)
         weightedDiff t0 t1 =
           let
             weight = Math.exp $ lambda * (Time.toSeconds $ t `Time.subtract` t1)
