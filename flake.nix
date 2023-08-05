49065f9f4d0b67aa4e35e88ac706393cdcf23271
Ruud van Asseldonk <dev@veniogames.com>
2023-08-05 18:18:29 +0200

    Convert Nix devenv to a flake based on 23.05
    I'm pinning most of my repositories to the same Nixpkgs version, so I
    only need a single copy of Nixpkgs in my store. This does break CI, I'll
    fix this in a follow-up commit.

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Sempervivum, a plant watering tracker";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      ghc = pkgs.ghc.withPackages (ps: [
+        ps.aeson
+        ps.tomland
+        ps.unordered-containers
+        ps.text
+      ]);
+
+      buildInputs = [
+        pkgs.git
+        pkgs.gnumake
+        pkgs.purescript   # Provides the Purescript compiler.
+        pkgs.psc-package  # Provides the Purescript package manager.
+
+        # Haskell toolchain for the utility that compiles the catalog.
+        ghc
+
+        # For minifying and optimizing the js output of the PureScript compiler.
+        pkgs.closurecompiler
+
+        # For compressing plant photos as WebP.
+        pkgs.libwebp
+      ];
+    in
+      {
+        # Note, we don't package Sempervivum as a Nix package directly, because
+        # I don't know how to make psc-package dependency fetching work with
+        # Nix.
+
+        devShells."${system}".default = pkgs.mkShell {
+          name = "sempervivum";
+
+          nativeBuildInputs = buildInputs;
+
+          # Needed for the locale-archive, to avoid LC_* errors.
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+        };
+      };
+}
--- nixpkgs-pinned.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/432fc2d9a67f92e05438dff5fdc2b39d33f77997.tar.gz";
-  sha256 = "0npj480pxjzl5vjsbijsn275z48jpqpjp0q5xbwpr3fx6m8lcvr6";
-})
