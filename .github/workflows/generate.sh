cdaf49ebfb3076e67089e9c46e749ebc97b7c287
Ruud van Asseldonk <dev@veniogames.com>
2024-02-25 18:23:13 +0100

    CI: Use new 'rcl build' feature to generate workflows

--- .github/workflows/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-echo "# Generated from *.rcl by generate.sh, do not edit manually." | tee build.yml deploy.yml
-rcl evaluate --format=json build.rcl >> build.yml
-rcl evaluate --format=json deploy.rcl >> deploy.yml
